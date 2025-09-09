e3707e508d578b1e5394d12b6467d44dd1c39fc8
Chie-codes <masawichiedza@gmail.com>
2025-09-09 14:37:40 +0100

    Add files via upload

--- hello_world.py
+++ hello_world.py
@@ -1,12 +1,1 @@
-# Request the user to input their name
-# Store input into a variable called name
-# Request the user to input their age
-# Store input into a variable called age
-# PRINT name
-# PRINT age
-# PRINT "Hello, World!"
-name= input("Please enter your name: ")
-age= input("Please enter your age: ")
-print("Name:", name)
-print("Age:", age)
-print("Hello, World! ")
+print("Git is awesome!")
